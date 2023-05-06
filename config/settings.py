8784d32ceb72f1804548fce30bff8371a23159b1
Mohamed Amarcha <simoamarcha@icloud.com>
2023-05-06 01:15:45 +0100

    'hhh'

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ import django_heroku
 import dj_database_url
 import os
 from dj_database_url import parse as dburl
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q77kmj1s5ne+!e_=j7^3q)&!qvq05ltl1ut##(b1mi&a6#l7gs'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG',default=False , cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
